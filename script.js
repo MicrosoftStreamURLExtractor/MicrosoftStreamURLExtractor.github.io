4acdd58b104b1347f1816736abe22cd39499422d
lorossi <lo.rossi@live.com>
2020-06-19 20:46:55 +0200

    now you can download links

--- script.js
+++ script.js
@@ -3,17 +3,29 @@
     let input_encoded, input_decoded, reader;
     let prefix = "https://web.microsoftstream.com";
     let output_element = $(".output-container #output");
+    let download_button = $(".output-container #download");
+    let download_file = $(".output-container #filedownload")
 
     reader = new FileReader();
     reader.onload = function(){
       input_encoded =  reader.result;
       input_decoded = atob(input_encoded.split(",")[1]);
       urls = extractUrls(input_decoded, prefix);
-      showUrls(urls, output_element);
+      cleanUrls(output_element, download_button);
+      showUrls(urls, output_element, download_button);
+      if (urls.length > 0) {
+        attachDownload(urls, download_file);
+      }
     };
 
     reader.readAsDataURL($(this).prop('files')[0]);
 });
+
+
+function cleanUrls(element, button) {
+  $(element).text("");
+  $(button).css("display", "none");
+}
 
 
 function extractUrls(text, prefix){
@@ -33,10 +45,29 @@ function extractUrls(text, prefix){
     return unique_urls;
 }
 
-function showUrls(urls, element) {
+
+function showUrls(urls, element, button) {
+  if (urls.length > 0) {
+    urls.forEach(function(item) {
+      $(element).append(`<li id="link"><a href="${item}">${item}</a></li>`);
+    });
+    $(button).css("display", "block");
+  } else {
+    $(element).append(`<li id="notfound">No links found in file</li>`);
+  }
+}
+
+function attachDownload(urls, element) {
+  let prefix = "data:text/plain;charset=UTF-8,";
+  let output = "";
+  let filename = "microsoft_streams_list.txt";
 
   urls.forEach(function(item) {
-    $(element).append(`<li><a href="${item}">${item}</a></li>`);
+    output += item + '\n';
   });
+
+
+  $(element).attr("href", prefix + output);
+  $(element).attr("download", filename);
 
 }
